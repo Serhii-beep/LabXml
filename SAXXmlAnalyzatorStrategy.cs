943cd14f25ee737324ebb84f9150eb6aacd7936a
Serhii-beep <63475732+Serhii-beep@users.noreply.github.com>
2020-11-10 21:53:51 +0200

    Add files via upload

---
+++ SAXXmlAnalyzatorStrategy.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Linq;
+
+namespace Xml_Lab
+{
+    class SAXXmlAnalyzatorStrategy : IXmlAnalyzatorStrategy
+    {
+        private string _path;
+        private XmlTextReader xmlTextReader;
+        public SAXXmlAnalyzatorStrategy(string path)
+        {
+            _path = path;
+            xmlTextReader = new XmlTextReader(_path);
+        }
+
+        public List<Member> Find(Member member)
+        {
+            List<Member> result = new List<Member>();           
+            Member memberTemp;
+            string cathedra = String.Empty;
+            string cathedraID = String.Empty;
+            string faculty = String.Empty;
+            bool toAdd = false;
+            while(xmlTextReader.Read())
+            {
+                if(xmlTextReader.Name == "Cathedra")
+                {
+                    while(xmlTextReader.MoveToNextAttribute())
+                    {
+                        if(xmlTextReader.Name == "CthdrName")
+                        {
+                            if(member.cathedra.cathedra == xmlTextReader.Value || member.cathedra.cathedra == String.Empty)
+                            {
+                                cathedra = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                xmlTextReader.Skip();
+                            }
+                        }
+                        if(xmlTextReader.Name == "Cthdr_ID")
+                        {
+                            if(member.cathedra.cathedraID == xmlTextReader.Value || member.cathedra.cathedraID == String.Empty)
+                            {
+                                cathedraID = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                xmlTextReader.Skip();
+                            }
+                        }
+                        if(xmlTextReader.Name == "FcltName")
+                        {
+                            if(member.cathedra.faculty == xmlTextReader.Value || member.cathedra.faculty == String.Empty)
+                            {
+                                faculty = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                xmlTextReader.Skip();
+                            }
+                        }
+                    }
+                }
+                if(xmlTextReader.Name == "Member")
+                {
+                    toAdd = true;
+                    memberTemp = new Member();
+                    while(xmlTextReader.MoveToNextAttribute())
+                    {
+                        if(xmlTextReader.Name == "Name")
+                        {
+                            if(member.name == xmlTextReader.Value || member.name == String.Empty)
+                            {
+                                memberTemp.name = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                toAdd = false;
+                                xmlTextReader.Skip();
+                            }
+                        }
+                        if(xmlTextReader.Name == "TimeOfEntry")
+                        {
+                            if(member.timeOfEntry == xmlTextReader.Value || member.timeOfEntry == String.Empty)
+                            {
+                                memberTemp.timeOfEntry = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                toAdd = false;
+                                xmlTextReader.Skip();
+                            }
+                        }
+                        if(xmlTextReader.Name == "Sex")
+                        {
+                            if(member.sex == xmlTextReader.Value || member.sex == String.Empty)
+                            {
+                                memberTemp.sex = xmlTextReader.Value;
+                            }
+                            else
+                            {
+                                toAdd = false;
+                                xmlTextReader.Skip();
+                            }
+                        }
+                    }
+                    if(toAdd)
+                    {
+                        memberTemp.cathedra.cathedra = cathedra;
+                        memberTemp.cathedra.cathedraID = cathedraID;
+                        memberTemp.cathedra.faculty = faculty;
+                        result.Add(memberTemp);
+                    }
+                }
+            }
+            xmlTextReader.Close();
+            return result;
+        }
+    }
+}
---
+++ Transformator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Xml_Lab
+{
+    class Transformator
+    {
+        private List<Member> members;
+        private string rootToTempXml = @"D:\C#\Xml_Lab\Xml_Lab\Temp.xml";
+        private string rootToXsl;
+        private string rootToOutputFile;
+        public Transformator(List<Member> _members, string _rootToXsl, string _rootToOutputFile)
+        {
+            members = _members;
+            rootToOutputFile = _rootToOutputFile;
+            rootToXsl = _rootToXsl;
+        }
+        public void Transform()
+        {
+            XslCompiledTransform xslt = new XslCompiledTransform();
+            xslt.Load(rootToXsl);
+            CreateXmlForTransforming();
+            string input = rootToTempXml;
+            string result = rootToOutputFile;
+            xslt.Transform(input, result);
+        }
+        private void CreateXmlForTransforming()
+        {
+            var xmlDoc = new XmlDocument();
+            xmlDoc.AppendChild(xmlDoc.CreateXmlDeclaration("1.0", "utf-8", null));
+            var root = xmlDoc.CreateElement("ScientificSociety");
+            xmlDoc.AppendChild(root);
+            if (members != null)
+            {
+                foreach (var m in members)
+                {
+                    XmlElement member = xmlDoc.CreateElement("Member");
+                    XmlAttribute name = xmlDoc.CreateAttribute("Name");
+                    XmlAttribute timeOfEntry = xmlDoc.CreateAttribute("TimeOfEntry");
+                    XmlAttribute sex = xmlDoc.CreateAttribute("Sex");
+                    XmlAttribute cthdrName = xmlDoc.CreateAttribute("CthdrName");
+                    XmlAttribute cthdrID = xmlDoc.CreateAttribute("Cthdr_ID");
+                    XmlAttribute fcltName = xmlDoc.CreateAttribute("FcltName");
+                    name.Value = m.name;
+                    timeOfEntry.Value = m.timeOfEntry;
+                    sex.Value = m.sex;
+                    cthdrName.Value = m.cathedra.cathedra;
+                    cthdrID.Value = m.cathedra.cathedraID;
+                    fcltName.Value = m.cathedra.faculty;
+                    member.Attributes.Append(name);
+                    member.Attributes.Append(timeOfEntry);
+                    member.Attributes.Append(sex);
+                    member.Attributes.Append(cthdrName);
+                    member.Attributes.Append(cthdrID);
+                    member.Attributes.Append(fcltName);
+                    root.AppendChild(member);
+                }
+            }
+            xmlDoc.Save(rootToTempXml);
+        }
+    }
+}
